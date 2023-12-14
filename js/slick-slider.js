a6a35057a062a89edd0c9c1c3f2495b3117762a6
Tyrone Mitchell <tyrone.mitchell@lippincott.com>
2023-12-14 18:13:16 -0500

    reformat the index.html file, add slick

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,12 +1,14 @@
 (function ($) {
-
-    $('.ew-slick').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        arrows : false
-    });
-
+	$(".methods-carousel").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true,
+		infinite: true,
+		speed: 2000,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		arrows: false,
+	});
 })(jQuery);
